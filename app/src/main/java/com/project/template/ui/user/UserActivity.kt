fdc7d314ff265464cd0d38a707a61d3e71efd519
baselvam <baselvam@paypal.com>
2022-02-20 10:57:44 +0530

    Splash screen and ui creation

--- app/src/main/java/com/project/template/ui/user/UserActivity.kt
+++ app/src/main/java/com/project/template/ui/user/UserActivity.kt
@@ -2,12 +2,14 @@ package com.project.template.ui.user
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.project.template.R
 
 class UserActivity : AppCompatActivity(R.layout.activity_user) {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
 
